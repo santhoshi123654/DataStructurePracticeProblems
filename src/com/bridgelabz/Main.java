94d174c469a477d0fe539953a7e9bc37834d5854
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-17 23:53:24 +0530

    Added:Main program

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LinkedList linkedlist = new LinkedList();
+		linkedlist.add(70);
+		linkedlist.add(30);
+		linkedlist.add(56);
+		
+		linkedlist.toPrint();
+	}
+}
