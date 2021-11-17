86f664dd469c47a92ccc516564f87bc418cbbf34
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-17 23:45:01 +0530

    Added:Linked List

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,48 @@
+package com.bridgelabz;
+
+public class LinkedList {
+	private Node head;
+	
+	private class Node{
+		int key;
+		Node next;
+		 
+	   public Node(int key){
+		this.key=key;
+			 
+	   }
+    }
+	public boolean add(int data) {
+		boolean isAdded =false;
+		Node newNode = new Node(data);
+			if(head == null) {
+				head = newNode;
+				isAdded = true;	
+			//56 is already added
+			}
+			else {
+				Node temp = head;
+				while(temp.next != null) {
+				temp = temp.next;
+				}
+				temp.next =newNode;
+				isAdded = true;
+			}
+			return isAdded;
+	}
+
+	public void print() {
+		if(head==null) {
+			System.out.println("linkedList is empty");
+		}else {
+			Node temp  = head;
+			System.out.println("LinkedList is : ");
+			while(temp!=null) {
+				System.out.print(temp.key + "-->");
+				temp=temp.next;
+			}
+		}
+		System.out.println();
+		
+	}
+}
