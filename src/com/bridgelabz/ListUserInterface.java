c0986cebe20e7d0cb56bdcba55f1bd859f642bc5
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-20 21:39:24 +0530

    Added: ListUserInterface Program

---
+++ src/com/bridgelabz/ListUserInterface.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class ListUserInterface {
+
+public int inputData() {
+		
+		System.out.println("enter the  element : ");
+		Scanner scanner = new Scanner(System.in);
+		int value = scanner.nextInt();
+		return value;	
+	}
+	
+	public int userMenu() {
+		
+		System.out.println(" enter 1 : for appending "+ "\n enter 2 : to push the element" 
+				+ "\n enter 3 : to insert at position"+ "\n enter 4 : to pop the element "
+				+"\n enter 5 : to pop at last "+ "\n enter 6 : for searching an element"
+			    + "\n enter 7 : to insert the value after  the searched value " 
+				+ "\n enter 8 : to delete the value "+" \n enter 0 :for exit program ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		return number;
+	}
+	
+}
